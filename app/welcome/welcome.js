d0df39e81269cd16166f405563e5e30cfbbbb743
cleriphil <cleri.phil@gmail.com>
2016-06-18 19:01:28 -0700

    change auction to list

--- app/welcome/welcome.js
+++ app/welcome/welcome.js
@@ -12,34 +12,28 @@ angular.module('myApp.welcome', ['ui.router', 'firebase'])
 
 .controller('WelcomeCtrl', ['$scope','CommonProp', '$firebaseArray', '$firebaseObject', function($scope, CommonProp, $firebaseArray, $firebaseObject ) {
   $scope.username = CommonProp.getUser();
-  var ref = new Firebase("https://art-app.firebaseio.com/auctions");
-  $scope.auctions = $firebaseArray(ref);
+  var ref = new Firebase("https://art-app.firebaseio.com/lists");
+  $scope.lists = $firebaseArray(ref);
 
 
 
   $scope.editAuction = function(id) {
-    var ref = new Firebase("https://art-app.firebaseio.com/auctions/" + id);
-    $scope.auctionToUpdate = $firebaseObject(ref);
+    var ref = new Firebase("https://art-app.firebaseio.com/lists/" + id);
+    $scope.listToUpdate = $firebaseObject(ref);
     $('#editModal').modal();
   }
 
   $scope.update = function() {
-    console.log($scope.auctionToUpdate.$id);
-    var fb = new Firebase("https://art-app.firebaseio.com/auctions/" + $scope.auctionToUpdate.$id);
-    var auction = $firebaseObject(fb);
+    console.log($scope.listToUpdate.$id);
+    var fb = new Firebase("https://art-app.firebaseio.com/lists/" + $scope.listToUpdate.$id);
+    var list = $firebaseObject(fb);
 
-    var startDate = $scope.auctionToUpdate.startDate.toString();
-    var startDate = new Date(startDate);
 
-    var endDate = $scope.auctionToUpdate.endDate.toString();
-    var endDate = new Date(endDate);
 
     fb.update({
-        title: $scope.auctionToUpdate.title,
-        description: $scope.auctionToUpdate.description,
-        startDate: startDate,
-        endDate: endDate,
-        emailId: $scope.auctionToUpdate.emailId
+        title: $scope.listToUpdate.title,
+        description: $scope.listToUpdate.description,
+        emailId: $scope.listToUpdate.emailId
     },
     function(error) {
       if (error) {
@@ -52,16 +46,16 @@ angular.module('myApp.welcome', ['ui.router', 'firebase'])
   };
 
   $scope.confirmDelete = function(id) {
-    var fb = new Firebase("https://art-app.firebaseio.com/auctions/" + id);
-    var auction = $firebaseObject(fb);
-    $scope.auctionToDelete = auction;
+    var fb = new Firebase("https://art-app.firebaseio.com/lists/" + id);
+    var list = $firebaseObject(fb);
+    $scope.listToDelete = list;
     $('#deleteModal').modal();
   }
 
   $scope.deleteAuction = function() {
-    var fb = new Firebase("https://art-app.firebaseio.com/auctions/" + $scope.auctionToDelete.$id);
-    var auction = $firebaseObject(fb);
-    auction.$remove()
+    var fb = new Firebase("https://art-app.firebaseio.com/lists/" + $scope.listToDelete.$id);
+    var list = $firebaseObject(fb);
+    list.$remove()
     .then(function(ref) {
         $('#deleteModal').modal('hide');
     }, function(error) {
